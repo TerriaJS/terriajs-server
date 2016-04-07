9374500c7ebb152c6a9f09336d2cc62a17c892c0
Kevin Ring <kevin@kotachrome.com>
2016-04-07 16:19:52 +1000

    Move some logic that was in bash to node instead.
    Also treat an empty list of proxyableDomains as "allow none" and if the property is unspecified treat it as if "proxyAllDomains" is true.

--- run_server.sh
+++ run_server.sh
@@ -2,24 +2,13 @@
 if [ -f "/etc/init.d/varnish" ]; then
     sudo /etc/init.d/varnish restart
 fi
-if [ -f "$(dirname $0)/terriajs.pid" ]; then
-    echo "Warning: server seems to be already running."
-fi
-date > output.log
+date > terriajs-server.log
 
 if [ "`which nohup`" == "" ]; then
     # There's no nohup on Windows. We just run node without it, which is fine in a dev environment.
-    node $(dirname "$0")/lib/app.js "$@" >> output.log 2> error.log < /dev/null &
+    (node $(dirname "$0")/lib/app.js "$@" 2>&1 < /dev/null) | tee -a terriajs-server.log &
 else
-    nohup node $(dirname "$0")/lib/app.js "$@" >> output.log 2> error.log < /dev/null &
+    (nohup node $(dirname "$0")/lib/app.js "$@" 2>&1 < /dev/null) | tee -a terriajs-server.log &
 fi
-sleep 2 # Give the server a chance to fail.
-cat output.log
-pid=$!
-ps | grep "^ *${pid}" > /dev/null
-running=$?
-if [ $running -eq 0 ]; then
-    echo "(TerriaJS-Server running in background with pid $!)." && echo $pid > "$(dirname $0)/terriajs.pid"
-else
-    cat error.log
-fi
+
+sleep 2
