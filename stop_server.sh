dc260ec5576470a5a7d166a74525cb6b67898a4c
Kevin Ring <kevin@kotachrome.com>
2018-10-09 21:50:03 +1100

    Remove unused file.

--- stop_server.sh
+++
@@ -1,7 +0,0 @@
-if [ -f "terriajs.pid" ]; then
-    pid=`cat "terriajs.pid"`
-    echo "(Killing old server)."
-    node -e "require('process').kill(${pid})"
-else
-    echo "TerriaJS server not running."
-fi
