9166ef85bd2cfbb01cc96e46481dd24dac12db15
Steve Bennett <stevage@gmail.com>
2018-12-13 17:15:18 +1100

    Document ecosystem options a bit.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,29 @@
 /*global __dirname*/
 const path = require('path');
 
+// This is a PM2 "ecosystem file", intended for a development environment.
+// For documentation on options, see https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+
 module.exports = {
   apps : [{
     name: path.basename(__dirname),
     script: 'lib/app.js',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // passed to app, so any valid arguments in options.js are allowed.
     args: '--port 3002',
+
+    // Increase the number of instances to support more concurrent users.
     instances: 1,
     autorestart: true,
+
+    // rather than setting watch:true here, use `npm run watch`
     watch: false,
     max_memory_restart: '1G',
+
     env: {
       NODE_ENV: 'development'
     },
+    // If the script is called with `--env production`, this environment variable gets set.
     env_production: {
       NODE_ENV: 'production'
     }
