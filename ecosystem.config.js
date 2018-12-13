5a29a893059575978dd1fb775dd9a031c162fa6c
Steve Bennett <stevage@gmail.com>
2018-12-13 18:54:01 +1100

    Pass through arguments from NPM correctly.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,19 @@ const path = require('path');
 // This is a PM2 "ecosystem file", intended for a development environment.
 // For documentation on options, see https://pm2.io/doc/en/runtime/reference/ecosystem-file/
 
+
+// Pass through additional arguments that might ultimately have come from
+// something like `npm run start -- --port 3009`
+const argpos = process.argv.indexOf('--');
+const args = argpos > -1 ? process.argv.slice(argpos + 1 ) : [];
+
 module.exports = {
   apps : [{
     name: path.basename(__dirname),
     script: 'lib/app.js',
 
     // passed to app, so any valid arguments in options.js are allowed.
-    args: '--port 3002',
+    args: args.join(' '),
 
     // Increase the number of instances to support more concurrent users.
     instances: 1,
