9f88ea00b981f54612cdecf83130667ad6b5396c
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2020-09-07 20:35:44 +1000

    ts working!

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ const args = argpos > -1 ? process.argv.slice(argpos + 1) : [];
 module.exports = {
   apps : [{
     name: path.basename(__dirname),
-    script: 'lib/app.js',
+    script: 'dist/app.js',
 
     // passed to app, so any valid arguments in options.js are allowed.
     args: args.join(' '),
