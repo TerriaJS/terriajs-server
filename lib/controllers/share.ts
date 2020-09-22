0b1fad4b726b971595478e38ca3ed44ba86f5c13
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2020-09-22 17:05:49 +1000

    fix ts version + s3 share conversion bug

--- lib/controllers/share.ts
+++ lib/controllers/share.ts
@@ -149,7 +149,8 @@ function resolveS3(serviceOptions: any, id: any) {
     .getObject(params)
     .promise()
     .then(function(data: any) {
-      return convertShareJson(data.Body);
+      console.log(data.Body.toString());
+      return convertShareJson(data.Body.toString());
     })
     .catch(function(e: any) {
       throw {
