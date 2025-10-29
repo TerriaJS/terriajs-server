0bb8754f9403935f47a709c38a2882dba83b90d9
Zoran Kokeza <zorankokeza95@gmail.com>
2025-10-29 22:48:50 +0100

    format codebase with prettier

--- spec/proj4lookup.spec.js
+++ spec/proj4lookup.spec.js
@@ -1,31 +1,31 @@
-var request = require('supertest');
-var makeserver = require('../lib/makeserver');
+var request = require("supertest");
+var makeserver = require("../lib/makeserver");
 
 var server;
 
-describe('proj4lookup', function() {
-    beforeEach(function() {
-        var options = require('../lib/options').init(true);
-        server = makeserver(options);
+describe("proj4lookup", function () {
+  beforeEach(function () {
+    var options = require("../lib/options").init(true);
+    server = makeserver(options);
+  });
+
+  describe("on get", function () {
+    it("should return a definition for EPSG:4326", function (done) {
+      request(server)
+        .get("/api/v1/proj4def/EPSG:4326")
+        .expect(200, "+proj=longlat +datum=WGS84 +no_defs")
+        .end(assert(done));
     });
 
-    describe('on get', function() {
-        it('should return a definition for EPSG:4326', function(done) {
-            request(server)
-                .get('/api/v1/proj4def/EPSG:4326')
-                .expect(200, '+proj=longlat +datum=WGS84 +no_defs')
-                .end(assert(done));
-        });
-
-        it('should 404 unknown projection', function(done) {
-            request(server)
-                .get('/api/v1/proj4def/EPSG:Notarealthing')
-                .expect(404)
-                .end(assert(done));
-        });
+    it("should 404 unknown projection", function (done) {
+      request(server)
+        .get("/api/v1/proj4def/EPSG:Notarealthing")
+        .expect(404)
+        .end(assert(done));
     });
+  });
 
-    function assert(done) {
-        return (err) => err ? done.fail(err) : done();
-    }
+  function assert(done) {
+    return (err) => (err ? done.fail(err) : done());
+  }
 });
--- spec/share.spec.js
+++ spec/share.spec.js
@@ -1,92 +1,88 @@
 "use strict";
 
-const makeServer = require('../lib/makeserver');
-const request = require('supertest');
-const nock = require('nock');
-const path = require('path');
+const makeServer = require("../lib/makeserver");
+const request = require("supertest");
+const nock = require("nock");
+const path = require("path");
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
-describe('share endpoint (integration, real controller)', function() {
-    const appOptions = {
-        wwwroot: "./spec/mockwwwroot",
-        hostName: "localhost",
-        port: "3001",
-        settings: {
-            shareUrlPrefixes: {
-                "g": {
-                    "service": "gist",
-                    "userAgent": "TerriaJS-Server",
-                    "gistFilename": "terriajs-server-catalog.json",
-                    "gistDescription": "TerriaJS Shared catalog"
-                }
-            },
-            newShareUrlPrefix: "g",
-            shareMaxRequestSize: "200kb"
+describe("share endpoint (integration, real controller)", function () {
+  const appOptions = {
+    wwwroot: "./spec/mockwwwroot",
+    hostName: "localhost",
+    port: "3001",
+    settings: {
+      shareUrlPrefixes: {
+        g: {
+          service: "gist",
+          userAgent: "TerriaJS-Server",
+          gistFilename: "terriajs-server-catalog.json",
+          gistDescription: "TerriaJS Shared catalog"
         }
-    };
-
-    function buildApp() {
-        const options = require('../lib/options').init(true);
-        const mergedOptions = Object.assign(options, appOptions);
-        return makeServer(mergedOptions);
+      },
+      newShareUrlPrefix: "g",
+      shareMaxRequestSize: "200kb"
     }
+  };
 
-    beforeEach(() => {
-        nock.cleanAll();
-    });
+  function buildApp() {
+    const options = require("../lib/options").init(true);
+    const mergedOptions = Object.assign(options, appOptions);
+    return makeServer(mergedOptions);
+  }
 
-    afterAll(() => {
-        nock.restore();
-    });
+  beforeEach(() => {
+    nock.cleanAll();
+  });
 
-    describe('POST /share', function() {
-        it('should return 201 and correct response for valid payload', function(done) {
-            // Mock GitHub Gist API
-            const fakeGistId = "123456";
-            nock('https://api.github.com')
-                .post('/gists')
-                .reply(201, {
-                    id: fakeGistId
-                });
+  afterAll(() => {
+    nock.restore();
+  });
 
-            const payload = { test: "me" };
+  describe("POST /share", function () {
+    it("should return 201 and correct response for valid payload", function (done) {
+      // Mock GitHub Gist API
+      const fakeGistId = "123456";
+      nock("https://api.github.com").post("/gists").reply(201, {
+        id: fakeGistId
+      });
 
-            request(buildApp())
-                .post('/share')
-                .send(payload)
-                .expect(201)
-                .expect('Content-Type', /json/)
-                .expect(res => {
-                    const actualUrl = res.body.url;
-                    const expectedPath = `/share/g-${fakeGistId}`;
-                    expect(res.body.id).toBe(`g-${fakeGistId}`);
-                    expect(res.body.path).toBe(expectedPath);
-                    expect(actualUrl.endsWith(expectedPath)).toBeTrue();
-                })
-                .end(function(err) {
-                    if (err) return done.fail(err);
-                    done();
-                });
-        });
+      const payload = { test: "me" };
 
-        it('should return 413 when payload exceeds shareMaxRequestSize', function(done) {
-            const largePayload = 'a'.repeat(250000); // 250KB
-            request(buildApp())
-                .post('/share')
-                .set('Content-Type', 'application/json')
-                .send(`{"data":"${largePayload}"}`)
-                .expect(413)
-                .expect('Content-Type', /text|plain/)
-                .expect(res => {
-                    expect(
-                        res.text.includes('Payload Too Large')
-                    ).toBeTrue();
-                })
-                .end(function(err) {
-                    if (err) return done.fail(err);
-                    done();
-                });
+      request(buildApp())
+        .post("/share")
+        .send(payload)
+        .expect(201)
+        .expect("Content-Type", /json/)
+        .expect((res) => {
+          const actualUrl = res.body.url;
+          const expectedPath = `/share/g-${fakeGistId}`;
+          expect(res.body.id).toBe(`g-${fakeGistId}`);
+          expect(res.body.path).toBe(expectedPath);
+          expect(actualUrl.endsWith(expectedPath)).toBeTrue();
+        })
+        .end(function (err) {
+          if (err) return done.fail(err);
+          done();
         });
     });
+
+    it("should return 413 when payload exceeds shareMaxRequestSize", function (done) {
+      const largePayload = "a".repeat(250000); // 250KB
+      request(buildApp())
+        .post("/share")
+        .set("Content-Type", "application/json")
+        .send(`{"data":"${largePayload}"}`)
+        .expect(413)
+        .expect("Content-Type", /text|plain/)
+        .expect((res) => {
+          expect(res.text.includes("Payload Too Large")).toBeTrue();
+        })
+        .end(function (err) {
+          if (err) return done.fail(err);
+          done();
+        });
+    });
+  });
 });
