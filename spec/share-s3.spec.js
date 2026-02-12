f1a9cd52293109e6eb2921d997c51563a21b442d
Zoran Kokeza <zorankokeza95@gmail.com>
2026-02-12 20:47:01 +0100

    add more tests

--- spec/share-s3.spec.js
+++ spec/share-s3.spec.js
@@ -9,9 +9,9 @@ import options from "../lib/options.js";
 describe("Share Module (e2e) - S3", () => {
   let localstackContainer;
 
-  function buildApp() {
+  function buildApp(settingsOverrides = {}) {
     const opts = options.init(true);
-    const mergedOptions = Object.assign(opts, {
+    const mergedOptions = Object.assign({}, opts, {
       wwwroot: "./spec/mockwwwroot",
       hostName: "localhost",
       port: "3001",
@@ -29,7 +29,8 @@ describe("Share Module (e2e) - S3", () => {
           }
         },
         newShareUrlPrefix: "s3",
-        shareMaxRequestSize: "200kb"
+        shareMaxRequestSize: "200kb",
+        ...settingsOverrides
       }
     });
     return makeServer(mergedOptions);
@@ -78,6 +79,47 @@ describe("Share Module (e2e) - S3", () => {
 
   it("should return 404 for non-existent share", async () => {
     await supertestReq(buildApp()).get("/share/s3-nonexistentid").expect(404);
+  });
+
+  it("should return 500 when saving to non-existent bucket", async () => {
+    const app = buildApp({
+      shareUrlPrefixes: {
+        s3: {
+          service: "s3",
+          region: "us-east-1",
+          bucket: "no-such-bucket",
+          endpoint: localstackContainer.getConnectionUri(),
+          accessKeyId: "test",
+          secretAccessKey: "test",
+          keyLength: 54,
+          forcePathStyle: true
+        }
+      }
+    });
+
+    const response = await supertestReq(app)
+      .post("/share")
+      .send({ data: "test content" })
+      .expect(500);
+
+    expect(response.body.message).toBeDefined();
+  });
+
+  it("should return 400 for unknown share prefix", async () => {
+    await supertestReq(buildApp()).get("/share/unknown-someid").expect(400);
+  });
+
+  it("should return 404 when newShareUrlPrefix is not configured", async () => {
+    const app = buildApp({ newShareUrlPrefix: undefined });
+
+    const response = await supertestReq(app)
+      .post("/share")
+      .send({ data: "test content" })
+      .expect(404);
+
+    expect(response.body.message).toContain(
+      "not been configured to generate new share URLs"
+    );
   });
 
   afterAll(async () => {
